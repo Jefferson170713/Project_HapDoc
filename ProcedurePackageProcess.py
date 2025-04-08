d21a0e57c59a0f9896feb091568ab18233a7d586
Jefferson HapDoc <jefferson.almeida05@aluno.ifce.edu.br>
2025-04-08 16:19:37 -0300

    modification - Modificação da função process_data

--- ProcedurePackageProcess.py
+++ ProcedurePackageProcess.py
@@ -9,47 +9,69 @@ class ProcedurePackageProcess:
         self.sigo_path = sigo_path
         self.output_path = output_path
 
+    # 1. Função para ler o arquivo principal e os arquivos auxiliares
     def load_data(self):
-        # Lendo o arquivo principal
+        # 1.2 - Lendo o arquivo principal 
+        print(f'Estapa - 1. Função para ler o arquivo principal e os arquivos auxiliares')
+        print(f'Estapa - 1.2 - Lendo o arquivo principal')
         self.df = pd.read_csv(self.file_path, sep=';', encoding='latin1', low_memory=False)
-        print(f'Quantidade de linhas e colunas: {self.df.shape}')
-
+        print(f'Colunas de self.df : {self.df.columns}')
+        print(f'Quantidade de linhas e colunas self.df: {self.df.shape}')
+        
         # Ajustando colunas
         self.df['CD_SERVIÇO_HONORARIO'] = self.df['CD_SERVIÇO_HONORARIO'].astype(str).str.replace('.0', '').str.zfill(8)
         self.df['CD_PROCEDIMENTO_TUSS'] = self.df['CD_PROCEDIMENTO_TUSS'].astype(str).str.replace('.0', '')
         self.df['CD_TIPO_ACOMODACAO'] = self.df['CD_TIPO_ACOMODACAO'].astype(str).str.replace('.0', '')
-
-        # Lendo o arquivo modelo
+        print(f'Etapa - 1.2 - Concluida')
+        # 1.3 - Lendo o arquivo df_cod
+        print(f'ETAPA - 1.3 - Lendo o arquivo df_cod')
         self.df_cod = pd.read_csv(self.model_path, sep=';', encoding='latin1', low_memory=False)
         self.df_cod.rename(columns={'COD_RENOMEACAO': 'CD_SERVIÇO_HONORARIO'}, inplace=True)
         self.df_cod['CD_SERVIÇO_HONORARIO'] = self.df_cod['CD_SERVIÇO_HONORARIO'].astype(str).str.replace('.0', '').str.zfill(8)
-
+        print(f'Colunas de self.df_cod : {self.df_cod.columns}')
+        print(f'Quantidade de linhas e colunas self.df_cod: {self.df_cod.shape}')
+        print(f'Estapa - 1.3 - Concluida')
         # Lendo o arquivo SIGO
+        print(f'Estapa - 1.4 - Lendo o arquivo SIGO')
         self.df_sigo = pd.read_csv(self.sigo_path, sep=';', encoding='latin1', low_memory=False)
+        self.df_sigo['ANO_TABELA'] = self.df_sigo['ANO_TABELA'].astype(str).str.replace('.0', '').str.zfill(8)
+        print(f'Colunas de self.df_sigo : {self.df_sigo.columns}')
+        print(f'Quantidade de linhas e colunas self.df_sigo: {self.df_sigo.shape}')
+        print(f'Estapa - 1.4 - Concluida')
 
+    # 2. - Processando as colunas do arquivo principal
     def process_data(self):
-        # Filtrando colunas necessárias
+        # 2.1 - Selecionando as (12) colunas que usaremos.
+        print(f'Etapa - 2 - Processando as colunas do arquivo principal')
+        print(f'Etapa - 2.1 - Selecionando as (12) colunas que usaremos')
         columns_selects = [
             'ANO_TABELA', 'CD_SERVIÇO_HONORARIO', 'CD_PROCEDIMENTO_TUSS', 'NM_SERV_HONORARIO', 'NM_PROCEDIMENTO_TUSS',
             'VALOR_PROPOSTO', 'CD_TIPO_ACOMODACAO', 'URGENCIA', 'ELETIVA', 'TAXAS', 'MATERIAL',
             'CONSULTA_HONORARIO', 'ANESTESISTA', 'AUXILIAR', 'CD_TIPO_REDE'
         ]
         self.df_filtered = self.df[columns_selects].copy()
+        print(f'Estapa - 2.1 - Concluida')
 
-        # Mapeando valores do SIGO
+        # 2.2 - Criamos um dicionário para mapear as chaves de busca e trazer os valores do item *`1.4`*.
+        print(f'Etapa - 2.2 - Criamos um dicionário para mapear as chaves de busca e trazer os valores do item *`1.4`*.')
         map_dict = dict(zip(self.df_sigo['ANO_TABELA'], self.df_sigo['DESCRICAO']))
+        print(f'Etapa - 2.2 - Concluida')
+        # 2.3 - Criamos um dicionário para mapear as chaves de busca e trazer os valores do item *`1.4`*.
+        print(f'Eatapa - 2.3 - Criamos um dicionário para mapear as chaves de busca e trazer os valores do item *`1.4`*.')
         self.df_filtered['ANO_TABELA'] = self.df_filtered['ANO_TABELA'].map(map_dict).fillna(self.df_filtered['ANO_TABELA'])
 
-        # Criando a coluna NOMENCLATURA
+        print(f'Eatapa - 2.4 - Substituindo os valores em df com base no dicionário.')
         self.df_filtered['NOMENCLATURA'] = np.where(
             self.df_filtered['CD_SERVIÇO_HONORARIO'].isin(self.df_cod['CD_SERVIÇO_HONORARIO']),
             self.df_filtered['NM_PROCEDIMENTO_TUSS'],
             self.df_filtered['NM_SERV_HONORARIO']
         )
-
         # Removendo duplicatas
         self.df_filtered.drop_duplicates(inplace=True)
         self.df_filtered.reset_index(drop=True, inplace=True)
+        print(f'Quantidade de linhas e Colunas de self.df_filtered : {self.df_filtered.shape}')
+        print(f'Colunas de self.df_filtered: {self.df_filtered.columns}')
+        print(f'Estapa - 2.4 - Concluída')
 
     def save_to_excel(self):
         # Salvando os dados processados em um arquivo Excel
--- README.md
+++ README.md
@@ -5,11 +5,17 @@
     - 1.2 Começamos com algumas tratativas nas colunas necessárias que precisamos.
         - Correção de 3 colunas com valores inteiros, removendo __'.0'__ inconsistência.
         - Selecionando somente as colunas necessárias(14) ao invés de 31 colunas.
-    - 1.3 - Leitura do arquivo `De Para` para Auxiliar a confecção do arquivo principal.
+    - 1.3 - Leitura do arquivo para Auxiliar na descrição dos procedimentos.
+        - Quando o código deste arquivo for encontrado no arquivo principal, seguirá certas regras específicas.
+        - Poderá trazer o nome do procedimento ou o nome do serviço de acordo com cada linha.
+    - 1.4 - Leitura do arquivo `De Para` para Auxiliar a confecção do arquivo principal.
         - Correção de 1 coluna com valore inteiro, removendo __'.0'__ inconsistência.
         - Formando um dicionário para criar um mecanismo de chave de busca.
         - Usar a nova coluna chave para auxiliar no material principal.
-    - 1.4 - Leitura do arquivo para Auxiliar na descrição dos proceddimentos.
-        - Quando o código deste arquivo for encontrado no arquivo principal, seguirá certas regras específicas.
-        - Poderá trazer o nome do procedimento ou o nome do serviço de acordo com cada linha.
+
+- 2 - Processando as colunas do arquivo principal
+    - 2.1 - Selecionando as (12) colunas que usaremos.
+    - 2.2 - Criamos um dicionário para mapear as chaves de busca e trazer os valores do item *`1.4`*.
+    - 2.3 - Substituindo os valores em df com base no dicionário.
+    - 2.4 - filtrar o que precisamos.
 ---
