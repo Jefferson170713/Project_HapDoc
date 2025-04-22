190da0194d45819174e5fcb2064cbac76271feda
Jefferson HapDoc <jefferson.almeida05@aluno.ifce.edu.br>
2025-04-22 13:51:42 -0300

    Adição da temttativa de layout para o app

---
+++ HapDoc_layout.py
@@ -0,0 +1,43 @@
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QIcon
+
+
+class HapDoc(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("HapDoc")
+        self.setGeometry(100, 100, 700, 400)  # Ajuste o tamanho da janela
+        self.setWindowIcon(QIcon("./ARQUIVOS/logo/logo.ico"))
+        self.initUI()
+
+    def initUI(self):
+        # Cria o QWebEngineView para renderizar o HTML
+        self.webview = QWebEngineView()
+
+        # Caminho para o arquivo HTML
+        path = os.path.abspath("./ARQUIVOS/layout/html/index_hapdoc.html")
+        self.webview.setUrl(QUrl.fromLocalFile(path))
+
+        # Adiciona o QWebEngineView à janela principal
+        layout = QVBoxLayout()
+        layout.addWidget(self.webview)
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+
+# Loop do programa em funcionamento
+def main():
+    app = QApplication(sys.argv)
+    window = HapDoc()
+    window.show()
+    sys.exit(app.exec_())
+
+
+# Verifica se o arquivo é executado diretamente
+if __name__ == "__main__":
+    main()
